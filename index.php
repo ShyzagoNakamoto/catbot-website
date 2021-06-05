c95621158402ae6f471a4d6c5db9e35a3bd3ed14
strawhatcrue1 <84302030+strawhatcrue1@users.noreply.github.com>
2021-06-05 15:29:55 +0700

    push

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php include('header.php'); ?>
+
+<h1 class="h1-kiri">Beralih ke CatBot sekarang juga.</h1>
+<img class="index-img" src="assets/images/logoCatBot.png">
+<h1 class="h1-kanan">Dapatkan sensasi spotify premium tapi gratis.</h1>
 <?php include('footer.php'); ?>

54e2954614fcc9aaea9ad3014950499783cf9f19
strawhatcrue1 <84302030+strawhatcrue1@users.noreply.github.com>
2021-06-05 16:05:06 +0700

    Front-End Index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php include('header.php'); ?>
 
-<h1 class="h1-kiri">Beralih ke CatBot sekarang juga.</h1>
-<img class="index-img" src="assets/images/logoCatBot.png">
-<h1 class="h1-kanan">Dapatkan sensasi spotify premium tapi gratis.</h1>
+<div class="container">
+    <h1 class="h1-kiri">Beralih ke CatBot sekarang juga.</h1>
+    <img class="index-img" src="assets/images/logoCatBot.png">
+    <h1 class="h1-kanan">Dapatkan sensasi spotify premium tapi gratis.</h1>
+</div>
+
 <?php include('footer.php'); ?>
